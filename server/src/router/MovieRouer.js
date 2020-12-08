96b3417fa454b7277b385bb8a0dd97b354ed345d
Alan Schio <6757777+schirrel@users.noreply.github.com>
2020-12-08 09:24:56 -0300

    Init movie router
    missing watched set nd watch reminder

---
+++ server/src/router/MovieRouer.js
@@ -0,0 +1,47 @@
+let router = require("express").Router();
+let middleware = require("./middleware/AuthMiddleware");
+const MovieService = require('../service/MovieService');
+
+this.service = new MovieService();
+
+router.use(middleware);
+
+router.post('/', async (req, res) => {
+	let dto = req.body;
+
+	try {
+	let entity = await  this.service.create(dto);	
+	if(entity.erro) {
+		res.status(500).send(entity);
+	} else	
+	res.send(entity);
+} catch(err) {
+	res.status(500).send(err.message);
+}
+});
+
+router.get('/search', async (req, res) => {
+	let list = await this.service.search(req.query);
+	res.send(list);
+});
+
+router.put('/:id', async (req, res) => {
+	let id = req.params.id;
+	let dto = req.body;
+  
+	try {
+		let entity = await this.service.update(dto, id);
+
+		if (entity.erro) {
+			res.status(500).send(entity);
+		} else
+			res.send(entity);
+	}
+	catch (err){
+		res.status(500).send(err);
+	}
+});
+
+// CREATED SET WATCHED AND WATCH REMINDER
+
+module.exports = router;
