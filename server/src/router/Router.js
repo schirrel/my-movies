30a78f66e4d87456fe7188f327c776b07cfb2635
Alan Schio <6757777+schirrel@users.noreply.github.com>
2020-12-08 08:30:14 -0300

    New router

--- server/src/router/Router.js
+++ server/src/router/Router.js
@@ -1,6 +1,6 @@
 const LOGGER = require('../utils/logger');
 const autenticacao = require('./middleware/AutenticacaoMiddleware');
-
+/*
 class Router {
 	constructor(app,isAuth) {
 
@@ -27,3 +27,66 @@ class Router {
 }
 
 module.exports = Router;
+*/
+
+
+
+const createRouter = function (service, isAuth){
+let router = require("express").Router();
+	
+if(isAuth) {
+router.use(autenticacao);
+}
+	
+	router.get('/search', async (req, res) => {
+	let list = await this.service.search(req.query);
+	res.send(list);
+});
+	router.get('/:id', async (req, res) => {
+	let id = req.params.id;
+  
+	try {
+		let entity = await this.service.get(id);
+		if (entity.erro) {
+			res.status(500).send(entity);
+		} else
+			res.send(entity);
+	}
+	catch (err){
+		res.status(500).send(err);
+	}
+});
+
+router.put('/:id', async (req, res) => {
+	let id = req.params.id;
+	let dto = req.body;
+  
+	try {
+		let entity = await this.service.update(dto, id);
+
+		if (entity.erro) {
+			res.status(500).send(entity);
+		} else
+			res.send(entity);
+	}
+	catch (err){
+		res.status(500).send(err);
+	}
+});
+	
+router.post('/', async (req, res) => {
+	let dto = req.body;
+
+	try {
+	let entity = await  this.service.create(dto);	
+	if(entity.erro) {
+		res.status(500).send(entity);
+	} else	
+	res.send(entity);
+} catch(err) {
+	res.status(500).send(err.message);
+}
+});
+	return router;
+}
+module.exports = createRouter;
