b74175ead132be225f43bc875015c47e8759b338
Alan Schio <6757777+schirrel@users.noreply.github.com>
2020-12-08 06:57:00 -0400

    Create Profile.js

---
+++ server/src/models/Profile.js
@@ -0,0 +1,12 @@
+const Model = require('@schirrel/pg-connection/Model');
+class Profile extends Model{
+	constructor(args = {}){
+	super("PROFILE")
+	this.addColumn('name', 'NAME');
+	this.addColumn('main', 'MAIN');
+	this.addColumn('user', 'USER');
+	this.setValues(args);
+	}
+}
+
+module.exports = User;
