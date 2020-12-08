bf2f4ff19b5d5a75f86f53f7addd5cdc82c6ba8b
Alan Schio <6757777+schirrel@users.noreply.github.com>
2020-12-08 06:59:27 -0400

    Create ProfileService.js

---
+++ server/src/service/ProfileService.js
@@ -0,0 +1,32 @@
+const Service = require('./Service');
+const ProfileRepository = require('../repository/ProfileRepository');
+const Profile = require('../models/Profile');
+
+class ProfileService extends Service {
+	constructor() {
+		super(ProfileRepository, Profile)
+	}
+
+	validateUniqueEmail() {
+		// TODO - Estou utilizando o erro do banco pela constraint de unique, mas caso de tempo vou implementar dupla validação
+	}
+	validateCreate(model) {
+		if (!model.name) {
+			throw Error('Name is required');
+		}
+		if (!model.user) {
+			throw Error('User is required');
+		}
+    //User utils isDefined
+		if (model.main == "undefined" || model.main == null) {
+		model.main= false;
+		}
+	}
+	async create(model) {
+		this.validateCreate(model);
+		return await super.create(model);
+	}
+}
+
+
+module.exports = ProfileService;
