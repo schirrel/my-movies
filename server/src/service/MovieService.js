d6df5f6f68aed78d1d9ac8d1a7cb7472f7f6a1c2
Alan Schio <6757777+schirrel@users.noreply.github.com>
2020-12-08 09:19:37 -0300

    Add MovieService

---
+++ server/src/service/MovieService.js
@@ -0,0 +1,29 @@
+const Service = require('./Service');
+const MovieRepository = require('../repository/MovieRepository');
+const Movie = require('../models/Movie');
+
+class MovieService extends Service {
+	constructor() {
+		super(MovieRepository, Movie)
+	}
+
+	validateCreate(model) {
+		if (!model.movieId) {
+			throw Error('Movie is required');
+		}
+		if (!model.profile) {
+			throw Error('Profile is required');
+		}
+    //User utils isDefined
+		if (model.watched == "undefined" || model.watched == null) {
+		model.watched= false;
+		}
+	}
+	async create(model) {
+		this.validateCreate(model);
+		return await super.create(model);
+	}
+}
+
+
+module.exports = MovieService;
