89ce236c6daccacba8ac9ea9d59399288aff69d4
Alan Schio <6757777+schirrel@users.noreply.github.com>
2020-12-10 10:45:15 -0300

    Create TMDBService.js

---
+++ app/src/services/TMDBService.js
@@ -0,0 +1,13 @@
+const url ="";
+const apiKey = "";
+
+
+Chave da API (v3 auth)
+2402a694b08bf99a6cd32b20e3ce58fc
+
+Exemplo de Requisição de API
+https://api.themoviedb.org/3/movie/550?api_key=2402a694b08bf99a6cd32b20e3ce58fc
+
+
+Token de Leitura da API (v4 auth)
+eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNDAyYTY5NGIwOGJmOTlhNmNkMzJiMjBlM2NlNThmYyIsInN1YiI6IjVmZDIyM2RmOGU4NzAyMDA0M2Q3ZjM5NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BzCGYdrSlg0b5VK9hN51w8FmlTeLHFkcgu4hhuqgsDA
