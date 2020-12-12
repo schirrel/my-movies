530da37599e53c090cd217ce7b7549dc7beafc51
Alan Schio <6757777+schirrel@users.noreply.github.com>
2020-12-12 07:11:31 -0400

    Remove unused on service

--- server/src/service/Service.js
+++ server/src/service/Service.js
@@ -8,14 +8,12 @@ class Service {
 		}
 		this.repository = new repository();
 		this.table = table;
-		this.logger = logger;
 	}
 	async get(id) {
 		return await this.repository.get(id);
 	}
 	async create(model) {
 		let result =  await this.repository.create(model);
-		console.log(result);
 		return result.rows[0];
 	}
 	async update(model) {
@@ -24,14 +22,11 @@ class Service {
 	}
 
 	async delete(id) {
-		// let entity = new this.table({id:id});
-		// return await entity.delete();
 		return await this.repository.delete(id);
 	}
 	async list() {
 		let result = await this.repository.list();
 		result = result.map(item => new this.table(item));
-
 		return result;
 	}
 
